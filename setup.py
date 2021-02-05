472a84e4fc9cf260a5f7311701bc35bde2a78d7b
Jayascript <6750308-jayascript@users.noreply.gitlab.com>
2021-02-05 00:25:34 -0500

    Switch to setuptools and fix istall_requires * Change from unordered dict to list

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 try:
     from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+    from setuptools import setup
 import os
 import sys
 
@@ -13,7 +13,9 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 setup(name='cycles',
       version='1.0',
       description='a command line utility for logging menstrual cycles',
-      install_requires={'python-dateutil>=2.1',},
+      install_requires=[
+          'python-dateutil>=2.1',
+      ],
       entry_points={
             'console_scripts': [
                 'cycles = cycles:cli',
